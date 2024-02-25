19cf144ed0a1ec7fb6816b911636b1358d2a182d
Vasilii <88195281+Vaskis23@users.noreply.github.com>
2024-02-25 21:39:26 +0200

    Add files via upload

---
+++ contacts.php
@@ -0,0 +1,21 @@
+<h1>My contact data</h1>
+
+<?php
+  $my_first_name = "Vasilii";
+  $my_last_name = "Panciuc";
+  $my_Age = 33;
+  $my_Gender = "male";
+  $my_Country = "Moldova";
+  $my_city = "Chisinau";
+  $my_street = "calea iesilor";
+  $my_phone = "076769997";
+  $my_email = 'vasilii.panciuc@simpals.com';
+?>
+
+<p>Hi my name is <?php print($my_first_name)?> <?php print($my_last_name)?><p>
+<p>And i have <?php print($my_Age) ?> year<p>
+<p>My gender <?php print($my_Gender) ?><p>
+<p>Im from <?php print($my_Country) ?> city <?php print($my_city) ?><p>
+<p>I live in the street <?php print($my_street) ?><p>
+<p>My phone number <?php print($my_phone) ?><p>
+<p>My email adress <?php print($my_email) ?><p>
---
+++ index.php
@@ -0,0 +1,8 @@
+<h1>Welcone to our HOTEL Booking</h1>
+<?php
+  $days_till_openning = 30;
+  $bonusPlaces = 10;
+?>
+<p>Days till opening <?php print($days_till_openning) ?><p>
+<p>Bonus places left <?php print($bonusPlaces) ?><p>
+<p>Don't hesitate to early book us!<p>
